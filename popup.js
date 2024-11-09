65997b965be239605717a3495dcc137c62aa28f5
Vadim Frolov <vadimf74@ya.ru>
2024-11-09 19:50:14 +0100

    added button and script

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('myButton');
+  button.addEventListener('click', function() {
+    // Perform the desired action here
+    alert('Button clicked!');
+  });
+});
